36ebedd5183b98864faaf886c7cbb71303e9f17a
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-08-19 23:53:50 -0300

    Aula Flappy Bird #02 finalizada

--- dom/dom/js/flappy.js
+++ dom/dom/js/flappy.js
@@ -27,4 +27,22 @@ function ParDeBarreiras(altura, abertura, x) {
     this.inferior = new Barreira(false)
 
     this.elemento.appendChild(this.superior.elemento)
+    this.elemento.appendChild(this.inferior.elemento)
+
+    this.sortearAbertura = () => {
+        const alturaSuperior = Math.random() * (altura - abertura)
+        const alturaInferior = altura - abertura - alturaSuperior
+        this.superior.setAltura(alturaSuperior)
+        this.inferior.setAltura(alturaInferior)
+    }
+
+    this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
+    this.setX = x => this.elemento.style.left = `${x}px`
+    this.getLargura = () => this.elemento.clientWidth
+
+    this.sortearAbertura()
+    this.setX(x)
 }
+
+const b = new ParDeBarreiras(700, 200, 400)
+document.querySelector('[wm-flappy]').appendChild(b.elemento)
