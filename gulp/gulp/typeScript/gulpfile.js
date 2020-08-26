b6b25dbb37b1861e812ded39e5a60acf3b58e8d8
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-08-25 23:26:48 -0300

    Aula O que é TypeScript finalizada

---
+++ gulp/gulp/typeScript/gulpfile.js
@@ -0,0 +1,7 @@
+// Typescript é um superSet de javascript, adicionando 
+//tipagem ao mesmo. Usaremos um plugin do gulp para poder
+//trabalhar com typescript.
+//Typescript adiciona outras coisas também, como interfaces. É
+//uma linguagem criada pela microsoft e podemos usá-lo junto
+//de grandes frameworks, e tanto para o backend quanto para
+//o frontend.
