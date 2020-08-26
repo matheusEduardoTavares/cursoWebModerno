4d55c15d7d3cc19fbe42eab752d929c4d2fee9df
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-08-25 23:55:36 -0300

    Aula O que é SASS finalizada

---
+++ gulp/gulp/css/gulpfile.js
@@ -0,0 +1,7 @@
+// SASS é um pré-processador CSS: conseguimos por novas
+//funcionalidades que não estão disponíveis no CSS para
+//nos ajudar a melhorar o código CSS, podendo reutilizá-lo
+//entre outros. Com ele conseguimos trabalhar com
+//variáveis. Temos vários recursos dentro do SASS. É
+//como um superSet de CSS, em que é colocado vários
+//recursos que não estão disponíveis no CSS padrão.
