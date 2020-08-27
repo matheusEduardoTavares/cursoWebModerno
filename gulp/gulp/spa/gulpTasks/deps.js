5c4be4cd6e39831da2d2694ae602101801f9810a
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-08-27 17:04:05 -0300

    Aula Projeto SPA: Tasks Dependências realizada

--- gulp/gulp/spa/gulpTasks/deps.js
+++ gulp/gulp/spa/gulpTasks/deps.js
@@ -2,15 +2,21 @@
 //JS com todas as dependências e teremos outro arquivo
 //JS com o código da aplicação.
 const gulp = require('gulp')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
 
 function depsCSS(callback) {
+    // return gulp.src('curso/gulp/gulp/spa/node_modules/font-awesome/css/font-awesome.css')
+    //     .pipe(uglifycss({ "uglyComments": false }))
+    //     .pipe(concat('deps.min.css'))
+    //     .pipe(gulp.dest('build/assets/css'))
 
     return callback()
 }
 
-function depsFonts(callback) {
-
-    return callback()
+function depsFonts() {
+    return gulp.src('curso/gulp/gulp/spa/node_modules/font-awesome/fonts/*.*')
+        .pipe(gulp.dest('build/assets/fonts'))
 }
 
 module.exports = {
