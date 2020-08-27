c67fc3f1c31c4c96c16e8a09f52db4fa322e85a0
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-08-26 23:48:24 -0300

    Aula Projeto SPA: Estrutura do Projeto finalizada

---
+++ gulp/gulp/spa/gulpfile.js
@@ -0,0 +1,5 @@
+// Faremos um pequeno projeto usando várias dependências
+//que estão no package.json. Usaremos um servidor para
+//servir nossos arquivos e sempre que algo mudar tudo 
+//será recompilado automaticamente. O build será quebrado
+//em vários arquivos ao invés de um arquivo só.
