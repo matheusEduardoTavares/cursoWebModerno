df35a7613576cbecdde5bec4110215dcab35448c
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-08-26 23:28:48 -0300

    Aula Provessando SASS com Gulp #01 finalizada

--- gulp/gulp/css/gulpfile.js
+++ gulp/gulp/css/gulpfile.js
@@ -5,3 +5,19 @@
 //variáveis. Temos vários recursos dentro do SASS. É
 //como um superSet de CSS, em que é colocado vários
 //recursos que não estão disponíveis no CSS padrão.
+const { series } = require('gulp')
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const concat = require('gulp-concat')
+const uglifycss = require('gulp-uglifycss')
+
+function transformacaoCSS(){
+    return gulp.src('src/sass/index.scss')
+        // Convertendo todo SASS para CSS:
+        .pipe(sass().on('error', sass.logError))
+        .pipe(uglifycss({ 'uglyComments': true }))
+        .pipe(concat('estilo.min.css'))
+        .pipe(gulp.dest('build/css'))
+}
+
+exports.default = series(transformacaoCSS)
